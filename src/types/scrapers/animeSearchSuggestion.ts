2046ca9a892c2b7c2444edd7d979b21773d05acf
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-08-06 14:02:12 +0530

    feat: remove HttpError union type

--- src/types/scrapers/animeSearchSuggestion.ts
+++ src/types/scrapers/animeSearchSuggestion.ts
@@ -1,6 +1,5 @@
-import type { HttpError } from "http-errors";
 import type { AnimeSearchSuggestion } from "../anime.js";
 
 export interface ScrapedAnimeSearchSuggestion {
-  suggestions: Array<AnimeSearchSuggestion> | HttpError;
+  suggestions: Array<AnimeSearchSuggestion>;
 }
