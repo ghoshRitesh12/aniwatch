9d5fe795475a4405bd072d44e5de9f013e3b13a8
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-12-17 20:32:41 +0530

    fix(vercel): try2 to fix 'cannot find module puppeteer-extra-plugin-stealth'
    taken from: https://github.com/vercel/pkg/issues/910#issuecomment-1032839383

--- src/extractors/megacloud.ts
+++ src/extractors/megacloud.ts
@@ -4,23 +4,23 @@ import puppeteer from "puppeteer-extra";
 import { HiAnimeError } from "../hianime/error.js";
 import { __dirname } from "../utils/constants.js";
 
-import "puppeteer-extra-plugin-stealth/evasions/chrome.app";
-import "puppeteer-extra-plugin-stealth/evasions/chrome.csi";
-import "puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes";
-import "puppeteer-extra-plugin-stealth/evasions/chrome.runtime";
-import "puppeteer-extra-plugin-stealth/evasions/defaultArgs"; // pkg warned me this one was missing
-import "puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow";
-import "puppeteer-extra-plugin-stealth/evasions/media.codecs";
-import "puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency";
-import "puppeteer-extra-plugin-stealth/evasions/navigator.languages";
-import "puppeteer-extra-plugin-stealth/evasions/navigator.permissions";
-import "puppeteer-extra-plugin-stealth/evasions/navigator.plugins";
-import "puppeteer-extra-plugin-stealth/evasions/navigator.vendor";
-import "puppeteer-extra-plugin-stealth/evasions/navigator.webdriver";
-import "puppeteer-extra-plugin-stealth/evasions/sourceurl";
-import "puppeteer-extra-plugin-stealth/evasions/user-agent-override";
-import "puppeteer-extra-plugin-stealth/evasions/webgl.vendor";
-import "puppeteer-extra-plugin-stealth/evasions/window.outerdimensions";
+import "puppeteer-extra-plugin-stealth/evasions/chrome.app/index.js";
+import "puppeteer-extra-plugin-stealth/evasions/chrome.csi/index.js";
+import "puppeteer-extra-plugin-stealth/evasions/chrome.loadTimes/index.js";
+import "puppeteer-extra-plugin-stealth/evasions/chrome.runtime/index.js";
+import "puppeteer-extra-plugin-stealth/evasions/defaultArgs/index.js"; // pkg warned me this one was missing
+import "puppeteer-extra-plugin-stealth/evasions/iframe.contentWindow/index.js";
+import "puppeteer-extra-plugin-stealth/evasions/media.codecs/index.js";
+import "puppeteer-extra-plugin-stealth/evasions/navigator.hardwareConcurrency/index.js";
+import "puppeteer-extra-plugin-stealth/evasions/navigator.languages/index.js";
+import "puppeteer-extra-plugin-stealth/evasions/navigator.permissions/index.js";
+import "puppeteer-extra-plugin-stealth/evasions/navigator.plugins/index.js";
+import "puppeteer-extra-plugin-stealth/evasions/navigator.vendor/index.js";
+import "puppeteer-extra-plugin-stealth/evasions/navigator.webdriver/index.js";
+import "puppeteer-extra-plugin-stealth/evasions/sourceurl/index.js";
+import "puppeteer-extra-plugin-stealth/evasions/user-agent-override/index.js";
+import "puppeteer-extra-plugin-stealth/evasions/webgl.vendor/index.js";
+import "puppeteer-extra-plugin-stealth/evasions/window.outerdimensions/index.js";
 
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 
