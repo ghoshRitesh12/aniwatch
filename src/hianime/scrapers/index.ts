ff6677fed9a8f517c254bc20057ae1cf15097503
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-12-21 22:52:14 +0530

    refactor: update scraper imports & exports

--- src/hianime/scrapers/index.ts
+++ src/hianime/scrapers/index.ts
@@ -1,16 +1,18 @@
 import { getHomePage } from "./homePage.js";
+import { getAZList } from "./animeAZList.js";
 import { getGenreAnime } from "./animeGenre.js";
-import { getAnimeSearchResults } from "./animeSearch.js";
 import { getAnimeEpisodes } from "./animeEpisodes.js";
 import { getAnimeCategory } from "./animeCategory.js";
 import { getProducerAnimes } from "./animeProducer.js";
 import { getEpisodeServers } from "./episodeServers.js";
 import { getAnimeAboutInfo } from "./animeAboutInfo.js";
+import { getAnimeSearchResults } from "./animeSearch.js";
 import { getEstimatedSchedule } from "./estimatedSchedule.js";
 import { getAnimeEpisodeSources } from "./animeEpisodeSrcs.js";
 import { getAnimeSearchSuggestion } from "./animeSearchSuggestion.js";
 
 export {
+  getAZList,
   getHomePage,
   getGenreAnime,
   getAnimeEpisodes,
