4d67d492d7084edaac1971db3a642c27a315949f
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-12-21 22:51:25 +0530

    feat: add `ScrapedAnimeAZList` scraper type

---
+++ src/hianime/types/scrapers/animeAZList.ts
@@ -0,0 +1,12 @@
+import type { AZListSortOptions } from "../anime.js";
+import type {
+  ScrapedAnimeCategory,
+  CommonAnimeScrapeTypes,
+} from "./animeCategory.js";
+
+export type ScrapedAnimeAZList = Pick<
+  ScrapedAnimeCategory,
+  CommonAnimeScrapeTypes
+> & {
+  azSortOption: AZListSortOptions;
+};
