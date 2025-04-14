187853be17a6bbcdcfc98ef360d5c7ff66a92422
Ritesh Ghosh <ghoshritesh55@gmail.com>
2025-04-14 23:13:36 +0530

    feat(getNextEpisodeSchedule): add scraper return type

--- src/hianime/types/scrapers/estimatedSchedule.ts
+++ src/hianime/types/scrapers/estimatedSchedule.ts
@@ -1,13 +1,19 @@
 type EstimatedSchedule = {
-  id: string | null;
-  time: string | null;
-  name: string | null;
-  jname: string | null;
-  airingTimestamp: number;
-  secondsUntilAiring: number;
-  episode: number;
+    id: string | null;
+    time: string | null;
+    name: string | null;
+    jname: string | null;
+    airingTimestamp: number;
+    secondsUntilAiring: number;
+    episode: number;
 };
 
 export type ScrapedEstimatedSchedule = {
-  scheduledAnimes: EstimatedSchedule[];
+    scheduledAnimes: EstimatedSchedule[];
+};
+
+export type ScrapedNextEpisodeSchedule = {
+    airingISOTimestamp: string | null;
+    airingTimestamp: number | null;
+    secondsUntilAiring: number | null;
 };
