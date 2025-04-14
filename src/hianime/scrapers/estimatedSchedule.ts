b9c5c6a5500509a7ef9243d3325d61d89afca947
Ritesh Ghosh <ghoshritesh55@gmail.com>
2025-04-14 23:13:49 +0530

    feat(getNextEpisodeSchedule): add `getNextEpisodeSchedule` method implementation

--- src/hianime/scrapers/estimatedSchedule.ts
+++ src/hianime/scrapers/estimatedSchedule.ts
@@ -1,61 +1,114 @@
 import { load, type CheerioAPI, type SelectorType } from "cheerio";
 import { client } from "../../config/client.js";
 import { HiAnimeError } from "../error.js";
-import { SRC_HOME_URL, SRC_AJAX_URL } from "../../utils/index.js";
+import { SRC_HOME_URL, SRC_AJAX_URL, SRC_BASE_URL } from "../../utils/index.js";
 import type { ScrapedEstimatedSchedule } from "../types/scrapers/index.js";
+import type { ScrapedNextEpisodeSchedule } from "../types/scrapers/estimatedSchedule.js";
 
 export async function getEstimatedSchedule(
-  date: string,
-  tzOffset: number = -330
+    date: string,
+    tzOffset: number = -330
 ): Promise<ScrapedEstimatedSchedule> {
-  const res: ScrapedEstimatedSchedule = {
-    scheduledAnimes: [],
-  };
-  try {
-    date = date?.trim();
-    if (date === "" || /^\d{4}-\d{2}-\d{2}$/.test(date) === false) {
-      throw new HiAnimeError(
-        "invalid date format",
-        getEstimatedSchedule.name,
-        400
-      );
+    const res: ScrapedEstimatedSchedule = {
+        scheduledAnimes: [],
+    };
+    try {
+        date = date?.trim();
+        if (date === "" || /^\d{4}-\d{2}-\d{2}$/.test(date) === false) {
+            throw new HiAnimeError(
+                "invalid date format",
+                getEstimatedSchedule.name,
+                400
+            );
+        }
+        const estScheduleURL =
+            `${SRC_AJAX_URL}/schedule/list?tzOffset=${tzOffset}&date=${date}` as const;
+        const mainPage = await client.get(estScheduleURL, {
+            headers: {
+                Accept: "*/*",
+                Referer: SRC_HOME_URL,
+                "X-Requested-With": "XMLHttpRequest",
+            },
+        });
+        const $: CheerioAPI = load(mainPage?.data?.html);
+        const selector: SelectorType = "li";
+        if ($(selector)?.text()?.trim()?.includes("No data to display")) {
+            return res;
+        }
+        $(selector).each((_, el) => {
+            const airingTimestamp = new Date(
+                `${date}T${$(el)?.find("a .time")?.text()?.trim()}:00`
+            ).getTime();
+            res.scheduledAnimes.push({
+                id: $(el)?.find("a")?.attr("href")?.slice(1)?.trim() || null,
+                time: $(el)?.find("a .time")?.text()?.trim() || null,
+                name:
+                    $(el)?.find("a .film-name.dynamic-name")?.text()?.trim() ||
+                    null,
+                jname:
+                    $(el)
+                        ?.find("a .film-name.dynamic-name")
+                        ?.attr("data-jname")
+                        ?.trim() || null,
+                airingTimestamp,
+                secondsUntilAiring: Math.floor(
+                    (airingTimestamp - Date.now()) / 1000
+                ),
+                episode: Number(
+                    $(el).find("a .fd-play button").text().trim().split(" ")[1]
+                ),
+            });
+        });
+        return res;
+    } catch (err: any) {
+        throw HiAnimeError.wrapError(err, getEstimatedSchedule.name);
     }
-    const estScheduleURL =
-      `${SRC_AJAX_URL}/schedule/list?tzOffset=${tzOffset}&date=${date}` as const;
-    const mainPage = await client.get(estScheduleURL, {
-      headers: {
-        Accept: "*/*",
-        Referer: SRC_HOME_URL,
-        "X-Requested-With": "XMLHttpRequest",
-      },
-    });
-    const $: CheerioAPI = load(mainPage?.data?.html);
-    const selector: SelectorType = "li";
-    if ($(selector)?.text()?.trim()?.includes("No data to display")) {
-      return res;
+}
+
+export async function getNextEpisodeSchedule(
+    animeId: string
+): Promise<ScrapedNextEpisodeSchedule> {
+    const res: ScrapedNextEpisodeSchedule = {
+        airingISOTimestamp: null,
+        airingTimestamp: null,
+        secondsUntilAiring: null,
+    };
+    try {
+        animeId = animeId?.trim();
+        if (!animeId || animeId.indexOf("-") === -1) {
+            throw new HiAnimeError(
+                "invalid anime id",
+                getNextEpisodeSchedule.name,
+                400
+            );
+        }
+
+        const animeUrl = `${SRC_BASE_URL}/watch/${animeId}` as const;
+        const mainPage = await client.get<string>(animeUrl, {
+            headers: {
+                Accept: "*/*",
+                Referer: SRC_HOME_URL,
+            },
+        });
+
+        const $: CheerioAPI = load(mainPage.data);
+        const selector: SelectorType =
+            ".schedule-alert > .alert.small > span:last";
+
+        const timestamp = String(
+            $(selector).attr("data-value")?.trim() || null
+        );
+        const schedule = new Date(timestamp);
+        if (isNaN(schedule.getTime())) return res;
+
+        res.airingISOTimestamp = schedule.toISOString();
+        res.airingTimestamp = schedule.getTime();
+        res.secondsUntilAiring = Math.floor(
+            (res.airingTimestamp - Date.now()) / 1000
+        );
+
+        return res;
+    } catch (err: any) {
+        throw HiAnimeError.wrapError(err, getNextEpisodeSchedule.name);
     }
-    $(selector).each((_, el) => {
-      const airingTimestamp = new Date(
-        `${date}T${$(el)?.find("a .time")?.text()?.trim()}:00`
-      ).getTime();
-      res.scheduledAnimes.push({
-        id: $(el)?.find("a")?.attr("href")?.slice(1)?.trim() || null,
-        time: $(el)?.find("a .time")?.text()?.trim() || null,
-        name: $(el)?.find("a .film-name.dynamic-name")?.text()?.trim() || null,
-        jname:
-          $(el)
-            ?.find("a .film-name.dynamic-name")
-            ?.attr("data-jname")
-            ?.trim() || null,
-        airingTimestamp,
-        secondsUntilAiring: Math.floor((airingTimestamp - Date.now()) / 1000),
-        episode: Number(
-          $(el).find("a .fd-play button").text().trim().split(" ")[1]
-        ),
-      });
-    });
-    return res;
-  } catch (err: any) {
-    throw HiAnimeError.wrapError(err, getEstimatedSchedule.name);
-  }
 }
