932c44e274bd9ed58c1ff99dfc6dfe253a16ffaa
Ritesh Ghosh <ghoshritesh55@gmail.com>
2025-04-14 23:11:58 +0530

    feat: add prettier and editor config

---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+/**
+ * @type {import("prettier").Config}
+ */
+export default {
+    semi: true,
+    tabWidth: 4,
+    useTabs: false,
+    printWidth: 80,
+    singleQuote: false,
+    arrowParens: "always",
+    trailingComma: "es5",
+    singleAttributePerLine: true,
+};
